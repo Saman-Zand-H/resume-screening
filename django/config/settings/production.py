1e3a5731ec9435e37492613123c5c93403598ff4
Ali Avani <ali.avani@outlook.com>
2023-08-03 00:38:46 +0330

    Fix production

--- django/config/settings/production.py
+++ django/config/settings/production.py
@@ -15,7 +15,6 @@ SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
-ALLOWED_HOSTS = [SITE_DOMAIN]
 
 # Redis
 CACHES = {
