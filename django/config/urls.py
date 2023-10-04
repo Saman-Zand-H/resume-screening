23493add3fa290914638a9e2c90bee933e6fba93
Ali Avani <ali.avani@outlook.com>
2023-10-04 18:30:22 +0330

    feature: add media serving for development

--- django/config/urls.py
+++ django/config/urls.py
@@ -1,6 +1,8 @@
 from apps.api.schema import schema
 from graphene_django.views import GraphQLView
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
@@ -9,3 +11,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
