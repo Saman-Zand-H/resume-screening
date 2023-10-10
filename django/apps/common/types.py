45d50f69fe30c1806420df478cfe5f1db6cafcf7
Amin Amerian <aam.amin.am@gmail.com>
2023-10-10 17:37:21 +0330

    feature: add UniversityType and FieldType

--- django/apps/common/types.py
+++ django/apps/common/types.py
@@ -1,8 +1,25 @@
 from graphene_django.types import DjangoObjectType
-from .models import Job
+from .models import Job, University, Field
 
 
 class JobType(DjangoObjectType):
     class Meta:
         model = Job
         fields = (Job.id.field.name, Job.title.field.name)
+
+
+class UniversityType(DjangoObjectType):
+    class Meta:
+        model = University
+        fields = (
+            University.id.field.name,
+            University.name.field.name,
+            University.city.field.name,
+            University.website.field.name,
+        )
+
+
+class FieldType(DjangoObjectType):
+    class Meta:
+        model = Field
+        fields = (Field.id.field.name, Field.name.field.name)
