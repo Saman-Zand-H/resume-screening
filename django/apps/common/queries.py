58e229e0843587b9d7559d70feea64ecd29ff040
Ali Avani <ali.avani@outlook.com>
2023-10-10 18:56:20 +0330

    feature: add cities light queries SCRUM-227

--- django/apps/common/queries.py
+++ django/apps/common/queries.py
@@ -1,14 +1,24 @@
 import graphene
-from .types import UniversityType, FieldType
-from .models import University, Field
+from graphene_django.filter import DjangoFilterConnectionField
+
+from .models import Field, University
+from .types import CityNode, FieldType, UniversityType
 
 
-class Query(graphene.ObjectType):
+class CommonQuery(graphene.ObjectType):
     universities = graphene.List(UniversityType)
     fields = graphene.List(FieldType)
+    cities = DjangoFilterConnectionField(CityNode, max_limit=15)
 
     def resolve_universities(self, info):
         return University.objects.all()
 
     def resolve_fields(self, info):
         return Field.objects.all()
+
+
+class Query(graphene.ObjectType):
+    common = graphene.Field(CommonQuery)
+
+    def resolve_common(self, info):
+        return CommonQuery()
--- django/apps/common/types.py
+++ django/apps/common/types.py
@@ -1,5 +1,12 @@
-from graphene_django.types import DjangoObjectType
-from .models import Job, University, Field
+from cities_light.graphql.types import City as CityTypeBase
+from cities_light.graphql.types import Country as CountryTypeBase
+from cities_light.graphql.types import Region as RegionTypeBase
+from cities_light.graphql.types import SubRegion as SubRegionTypeBase
+from cities_light.models import City, Country, Region, SubRegion
+from graphene import relay
+from query_optimizer import DjangoObjectType
+
+from .models import Field, Job, University
 
 
 class JobType(DjangoObjectType):
@@ -23,3 +30,32 @@ class FieldType(DjangoObjectType):
     class Meta:
         model = Field
         fields = (Field.id.field.name, Field.name.field.name)
+
+
+class CountryType(DjangoObjectType):
+    class Meta:
+        model = Country
+        interfaces = (relay.Node,)
+        fields = list(CountryTypeBase._meta.fields.keys())
+
+
+class RegionType(DjangoObjectType):
+    class Meta:
+        model = Region
+        fields = list(RegionTypeBase._meta.fields.keys())
+
+
+class SubRegionType(DjangoObjectType):
+    class Meta:
+        model = SubRegion
+        fields = list(SubRegionTypeBase._meta.fields.keys())
+
+
+class CityNode(DjangoObjectType):
+    class Meta:
+        model = City
+        interfaces = (relay.Node,)
+        fields = list(CityTypeBase._meta.fields.keys())
+        filter_fields = {
+            City.display_name.field.name: ["icontains"],
+        }
