753e83b3138356f3f340ec52c5e233b2a1670b78
Amin Amerian <aam.amin.am@gmail.com>
2023-10-13 13:31:48 +0330

    feature: add EducationType

--- django/apps/account/types.py
+++ django/apps/account/types.py
@@ -2,7 +2,7 @@ import graphene
 from common.types import JobType
 from query_optimizer import DjangoObjectType
 
-from .models import Profile
+from .models import Profile, Education
 
 
 class ProfileType(DjangoObjectType):
@@ -22,3 +22,20 @@ class ProfileType(DjangoObjectType):
 
     def resolve_job(self, info):
         return self.job.all()
+
+
+
+class EducationType(DjangoObjectType):
+    class Meta:
+        model = Education
+        fields = (
+            Education.id.field.name,
+            Education.field.field.name,
+            Education.degree.field.name,
+            Education.university.field.name,
+            Education.start.field.name,
+            Education.end.field.name,
+            Education.status.field.name,
+            Education.created_at.field.name,
+            Education.updated_at.field.name,
+        )
