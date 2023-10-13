6ad4951d350a783a6410d170aa196a1f81cb55f3
Amin Amerian <aam.amin.am@gmail.com>
2023-10-13 13:35:49 +0330

    feature: add mutations in common.mutations to schema

--- django/apps/api/schema.py
+++ django/apps/api/schema.py
@@ -2,13 +2,14 @@ import graphene
 from account.mutations import Mutation as AccountMutation
 from account.queries import Query as AccountQuery
 from common.queries import Query as CommonQuery
+from common.mutations import Mutation as CommonMutation
 
 
 class Query(AccountQuery, CommonQuery, graphene.ObjectType):
     pass
 
 
-class Mutation(AccountMutation, graphene.ObjectType):
+class Mutation(AccountMutation, CommonMutation, graphene.ObjectType):
     pass
 
 
