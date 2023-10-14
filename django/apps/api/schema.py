73e572bd1dce4c496ec6d46a29009a35526625c5
Ali Avani <ali.avani@outlook.com>
2023-10-14 19:33:53 +0330

    fix: remove common mutation import

--- django/apps/api/schema.py
+++ django/apps/api/schema.py
@@ -2,14 +2,15 @@ import graphene
 from account.mutations import Mutation as AccountMutation
 from account.queries import Query as AccountQuery
 from common.queries import Query as CommonQuery
-from common.mutations import Mutation as CommonMutation
+
+# from common.mutations import Mutation as CommonMutation
 
 
 class Query(AccountQuery, CommonQuery, graphene.ObjectType):
     pass
 
 
-class Mutation(AccountMutation, CommonMutation, graphene.ObjectType):
+class Mutation(AccountMutation, graphene.ObjectType):
     pass
 
 
