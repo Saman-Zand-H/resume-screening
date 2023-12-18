789564b46fb7fce5886def548fc2ccd842b104ec
Ali Avani <ali.avani@outlook.com>
2023-12-18 12:04:31 +0330

    fix: serve media files

--- django/config/urls.py
+++ django/config/urls.py
@@ -2,14 +2,13 @@ from apps.api.schema import schema
 from graphene_file_upload.django import FileUploadGraphQLView as GraphQLView
 
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.views.decorators.csrf import csrf_exempt
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
