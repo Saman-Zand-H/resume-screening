1f1825734bbf59e66dfae02b54fe7484536b24d6
Amin Amerian <aam.amin.am@gmail.com>
2023-12-18 11:49:13 +0330

    build: add media url to non-debug mode

--- django/config/urls.py
+++ django/config/urls.py
@@ -12,5 +12,4 @@ urlpatterns = [
     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
