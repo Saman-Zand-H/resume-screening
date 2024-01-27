0026b72bb7110ebd7a29d65f2d222a02a897a481
Amin Amerian <aam.amin.am@gmail.com>
2024-01-27 15:09:03 +0330

    refactor: replace model forms with pydantic in account.serializers SCRUM-400

--- django/apps/account/serializers.py
+++ django/apps/account/serializers.py
@@ -1,105 +1,159 @@
-from django import forms
-from django.forms import ModelForm
+from pydantic import BaseModel, field_validator, ValidationInfo
+from typing import List, Optional
+from datetime import date
+from enum import Enum
 
-from .models import User, Profile, Contact, Education, WorkExperience, LanguageCertificate, CertificateAndLicense
+from django.core.validators import URLValidator
+from phonenumber_field.validators import validate_phonenumber
 
-
-class UserForm(ModelForm):
-    class Meta:
-        model = User
-        fields = [User.gender.field.name, User.birth_date.field.name, User.skills.field.name]
-
-
-class ProfileForm(ModelForm):
-    class Meta:
-        model = Profile
-        fields = [
-            Profile.user.field.name,
-            Profile.height.field.name,
-            Profile.weight.field.name,
-            Profile.skin_color.field.name,
-            Profile.hair_color.field.name,
-            Profile.eye_color.field.name,
-            Profile.full_body_image.field.name,
-            Profile.employment_status.field.name,
-            Profile.interested_jobs.field.name,
-            Profile.city.field.name,
-        ]
+from .validators import LinkedInUsernameValidator, WhatsAppValidator
 
 
-class ContactForm(ModelForm):
-    class Meta:
-        model = Contact
-        fields = [
-            Contact.type.field.name,
-            Contact.value.field.name,
-        ]
+class Gender(str, Enum):
+    MALE = "male"
+    FEMALE = "female"
+    NOT_KNOWN = "not_known"
+    NOT_APPLICABLE = "not_applicable"
 
 
-class BaseDocumentForm(ModelForm):
-    # TODO: Make status not required in Meta class
-    status = forms.CharField(required=False)
-
-    class Meta:
-        abstract = True
-
-    @classmethod
-    def get_field_names(cls, model):
-        return [field.name for field in model._meta.fields if field.editable]
-
-
-class EducationForm(BaseDocumentForm):
-    class Meta:
-        model = Education
-        # TODO:add fields base on model in Meta class of super calss
-        fields = BaseDocumentForm.get_field_names(Education)
+class SkinColor(str, Enum):
+    VERY_FAIR = "#FFDFC4"
+    FAIR = "#F0D5B1"
+    LIGHT = "#E5B897"
+    LIGHT_MEDIUM = "#D9A377"
+    MEDIUM = "#C68642"
+    OLIVE = "#A86B33"
+    BROWN = "#8D5524"
+    DARK_BROWN = "#60391C"
+    VERY_DARK = "#3B260B"
+    DEEP = "#100C08"
 
 
-class WorkExperienceForm(BaseDocumentForm):
-    class Meta:
-        model = WorkExperience
-        fields = BaseDocumentForm.get_field_names(WorkExperience)
+class EyeColor(str, Enum):
+    AMBER = "#FFBF00"
+    BLUE = "#5DADEC"
+    BROWN = "#6B4226"
+    GRAY = "#BEBEBE"
+    GREEN = "#1CAC78"
+    HAZEL = "#8E7618"
 
 
-class LanguageCertificateForm(BaseDocumentForm):
-    class Meta:
-        model = LanguageCertificate
-        fields = BaseDocumentForm.get_field_names(LanguageCertificate)
+class EmploymentStatus(str, Enum):
+    EMPLOYED = "employed"
+    UNEMPLOYED = "unemployed"
 
 
-class CertificateAndLicenseForm(BaseDocumentForm):
-    class Meta:
-        model = CertificateAndLicense
-        fields = BaseDocumentForm.get_field_names(CertificateAndLicense)
+class ContactType(str, Enum):
+    WEBSITE = "website"
+    ADDRESS = "address"
+    LINKEDIN = "linkedin"
+    WHATSAPP = "whatsapp"
+    PHONE = "phone"
 
 
-class ResumeForm:
-    def __init__(self, data=None):
-        self.data = data or {}
-        self.errors = {}
+class Degree(str, Enum):
+    BACHELORS = "bachelors"
+    MASTERS = "masters"
+    PHD = "phd"
+    ASSOCIATE = "associate"
+    DIPLOMA = "diploma"
+    CERTIFICATE = "certificate"
 
-    def validate(self):
-        form_classes = {
-            "user": UserForm,
-            "profile": ProfileForm,
-            "contact": ContactForm,
-            "education": EducationForm,
-            "work_experience": WorkExperienceForm,
-            "language_certificate": LanguageCertificateForm,
-            "certificate_and_license": CertificateAndLicenseForm,
+
+class DocumentStatus(str, Enum):
+    DRAFTED = "drafted"
+    SUBMITTED = "submitted"
+    REJECTED = "rejected"
+    VERIFIED = "verified"
+    SELF_VERIFIED = "self_verified"
+
+
+class UserModel(BaseModel):
+    gender: Optional[Gender] = None
+    birth_date: Optional[date] = None
+    skills: Optional[List[int]] = None
+
+
+class ProfileModel(BaseModel):
+    user_id: int
+    height: Optional[int] = None
+    weight: Optional[int] = None
+    skin_color: Optional[SkinColor] = None
+    hair_color: Optional[str] = None
+    eye_color: Optional[EyeColor] = None
+    full_body_image: Optional[str] = None
+    employment_status: Optional[EmploymentStatus] = None
+    interested_jobs: Optional[List[int]] = None
+    city_id: Optional[int] = None
+
+
+class ContactModel(BaseModel):
+    type: ContactType
+    value: str
+
+    @field_validator("value")
+    def validate_value(cls, value, info: ValidationInfo):
+        contact_type = info.data.get("type")
+        validators = {
+            ContactType.PHONE: validate_phonenumber,
+            ContactType.WEBSITE: URLValidator(),
+            ContactType.LINKEDIN: LinkedInUsernameValidator(),
+            ContactType.WHATSAPP: WhatsAppValidator(),
         }
+        validator = validators.get(contact_type)
+        if validator:
+            validator(value)
+        return value
 
-        for form_name, form_class in form_classes.items():
-            form_data = self.data.get(form_name, [] if form_name == "contact" else {})
-            if form_name == "contact":
-                for data in form_data:
-                    f = form_class(data=data)
-                    if not f.is_valid():
-                        self.errors[form_name] = f.errors.get_json_data(escape_html=True)
-            else:
-                f = form_class(data=form_data)
-                if not f.is_valid():
-                    self.errors[form_name] = f.errors.get_json_data(escape_html=True)
 
-    def get_errors(self):
-        return self.errors
+class EducationModel(BaseModel):
+    user_id: int
+    status: Optional[DocumentStatus] = None
+    field_id: int
+    degree: Degree
+    university_id: int
+    start: date
+    end: Optional[date] = None
+
+
+class WorkExperienceModel(BaseModel):
+    user_id: int
+    status: Optional[DocumentStatus] = None
+    job_id: int
+    start: date
+    end: Optional[date] = None
+    organization: str
+    city_id: int
+
+
+class LanguageCertificateModel(BaseModel):
+    user_id: int
+    status: Optional[DocumentStatus] = None
+    language_id: int
+    test_id: int
+    issued_at: date
+    expired_at: date
+    listening_score: float
+    reading_score: float
+    writing_score: float
+    speaking_score: float
+    band_score: float
+
+
+class CertificateAndLicenseModel(BaseModel):
+    user_id: int
+    status: Optional[DocumentStatus] = None
+    title: str
+    certifier: str
+    issued_at: date
+    expired_at: Optional[date] = None
+
+
+class ResumeModel(BaseModel):
+    user: Optional[UserModel]
+    profile: Optional[ProfileModel]
+    contact: Optional[List[ContactModel]] = []
+    education: Optional[List[EducationModel]] = []
+    work_experience: Optional[List[WorkExperienceModel]] = []
+    language_certificate: Optional[List[LanguageCertificateModel]] = []
+    certificate_and_license: Optional[List[CertificateAndLicenseModel]] = []
