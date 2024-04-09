6dfc6b77ed151486df6a41715ef2bcff1fdf3f20
Saman Zand Haghighi <samanzandh@gmail.com>
2024-04-09 16:23:11 +0330

    refactor: use flex packages

--- django/apps/flex_pubsub/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class FlexPubsubConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "flex_pubsub"
-
-    def ready(self):
-        from . import checks  # noqa: F401
