ff02c2713fc01aa147ae5c7fc8a74e282c9a1778
Ali Avani <ali.avani@outlook.com>
2024-05-09 18:12:45 +0330

    fix: update CourseResult model status default value and enhance CourseResultCreateMutation

--- django/apps/academy/mutations.py
+++ django/apps/academy/mutations.py
@@ -1,27 +1,30 @@
 import graphene
+from account.mixins import DocumentCUDMixin
+from graphene_django_cud.mutations import DjangoCreateMutation
 
-from .models import Course
-from common.models import Industry
+from .models import CourseResult
 
 
-class StartCourseMutation(graphene.Mutation):
-    class Arguments:
-        course_id = graphene.ID()
+class CourseResultCreateMutation(DocumentCUDMixin, DjangoCreateMutation):
+    start_url = graphene.String(required=True)
 
-    url = graphene.String()
+    class Meta:
+        model = CourseResult
+        fields = (CourseResult.course.field.name,)
 
     @classmethod
-    def mutate(cls, root, info, course_id):
-        user = info.context.user
-        if user:
-            jobs = user.profile.interested_jobs.all()
-            if Industry.objects.filter(jobcategory__job__in=jobs, course=course_id).exists():
-                return StartCourseMutation(url="https://www.google.com")
-        return StartCourseMutation(url=None)
+    def before_create_obj(cls, info, input, obj):
+        obj.user = info.context.user
+        cls.full_clean(obj)
+
+    @classmethod
+    def after_mutate(cls, root, info, input, obj, return_data):
+        return_data["start_url"] = "https://example.com"
+        return super().after_mutate(root, info, input, obj, return_data)
 
 
 class AcademyMutation(graphene.ObjectType):
-    start_course = StartCourseMutation.Field()
+    start_course = CourseResultCreateMutation.Field()
 
 
 class Mutation(graphene.ObjectType):
