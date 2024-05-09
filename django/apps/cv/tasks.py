49e7e5459af1c4f140b48ae3ceccb5adf594495a
Saman Zand Haghighi <samanzandh@gmail.com>
2024-05-09 18:47:16 +0330

    feature: add generate resume task

--- django/apps/cv/tasks.py
+++ django/apps/cv/tasks.py
@@ -1,14 +1,28 @@
+from account.models import UserTask
 from config.settings.subscriptions import CVSubscription
 from flex_pubsub.tasks import register_task
 
-from django.core.files.base import ContentFile
-from django.core.files.storage import default_storage
+from django.contrib.auth import get_user_model
 
-from .models import CVTemplate
+from .models import GeneratedCV
 
 
 @register_task(subscriptions=[CVSubscription.CV])
-def render_cv_template(template_id: int, context: dict, target_file_name: str):
-    template = CVTemplate.objects.get(pk=template_id)
-    rendered_content = template.render_pdf(context, target_file_name)
-    default_storage.save(target_file_name, ContentFile(rendered_content))
+def render_cv_template(user_id: int, template_id: int = None):
+    user = get_user_model().objects.get(pk=user_id)
+    user_task = UserTask.objects.get_or_create(user=user, task_name=render_cv_template.__name__)[0]
+    if user_task.status == UserTask.TaskStatus.IN_PROGRESS:
+        return False
+
+    if not hasattr(user, "profile"):
+        user_task.change_status(UserTask.TaskStatus.FAILED)
+        return False
+
+    user_task.change_status(UserTask.TaskStatus.IN_PROGRESS)
+    try:
+        GeneratedCV.from_user(user_id, template_id)
+        user_task.change_status(UserTask.TaskStatus.COMPLETED)
+        return True
+    except Exception:
+        user_task.change_status(UserTask.TaskStatus.FAILED)
+        return False
