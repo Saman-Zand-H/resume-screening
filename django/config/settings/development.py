c8a6a9dc91664826e93a7b644c14ffced39d862d
Saman Zand Haghighi <samanzandh@gmail.com>
2024-05-11 00:36:56 +0330

    refactor: delete strict subscriptions for development

--- django/config/settings/development.py
+++ django/config/settings/development.py
@@ -1,6 +1,4 @@
 from .constants import Environment
 from .production import *  # noqa
-from .subscriptions import AccountSubscription
 
 ENVIRONMENT_NAME = Environment.DEVELOPMENT
-PUBSUB_SETTINGS["SUBSCRIPTIONS"] = ",".join([AccountSubscription.EMAILING.value])  # noqa
