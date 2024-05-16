1aacfc5e60187cf6c67ada82e39b5c5b0ec1b6d6
Saman Zand Haghighi <samanzandh@gmail.com>
2024-05-16 18:36:33 +0330

    refactor: update migrations

--- django/config/settings/local.py
+++ django/config/settings/local.py
@@ -13,9 +13,3 @@ SESSION_ENGINE = "django.contrib.sessions.backends.db"
 
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = os.environ.get("EMAIL_HOST")
-EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = True
