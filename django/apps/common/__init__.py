c0ba161af8970d9a880419e579f1ed92781ba014
Saman Zand Haghighi <samanzandh@gmail.com>
2024-05-29 19:56:31 +0330

    feat: enhance job and skill extraction with additional user information

--- django/apps/common/__init__.py
+++ django/apps/common/__init__.py
@@ -1,3 +1,5 @@
+import json
+
 from django_filters import BaseCSVFilter, CharFilter
 from graphene_django.filter.filterset import (
     FILTER_FOR_DBFIELD_DEFAULTS,
@@ -6,6 +8,8 @@ from graphene_django.filter.filterset import (
 )
 
 from django.contrib.postgres.fields import ArrayField
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db.models import QuerySet
 
 
 class CharArrayFilter(BaseCSVFilter, CharFilter):
@@ -21,3 +25,26 @@ def __monkeypatch_graphene_filterset_mixin__():
 
 
 __monkeypatch_graphene_filterset_mixin__()
+
+
+class DjangoQuerysetEncoder(DjangoJSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, QuerySet):
+            return list(obj)
+
+        if hasattr(obj, "__dict__"):
+            return {key: value for key, value in obj.__dict__.items() if not key.startswith("_")}
+
+        return super().default(obj)
+
+
+original_dumps = json.dumps
+
+
+def patched_dumps(obj, *args, **kwargs):
+    if "cls" not in kwargs:
+        kwargs["cls"] = DjangoQuerysetEncoder
+    return original_dumps(obj, *args, **kwargs)
+
+
+json.dumps = patched_dumps
