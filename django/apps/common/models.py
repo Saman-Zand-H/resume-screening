c5d90cf8de1a5f841b12d535520eda1e64acea58
Ali Avani <ali.avani@outlook.com>
2024-06-05 18:00:39 +0330

    feature: add order field to Job model and update admin display; enhance available_jobs resolver in ProfileType SCRUM-1052

--- django/apps/common/models.py
+++ django/apps/common/models.py
@@ -38,10 +38,12 @@ class Job(models.Model):
     title = models.CharField(max_length=255, verbose_name=_("Title"))
     category = models.ForeignKey(JobCategory, on_delete=models.CASCADE, verbose_name=_("Category"))
     require_appearance_data = models.BooleanField(default=False, verbose_name=_("Require Appearance Data"))
+    order = models.PositiveIntegerField(default=0, verbose_name=_("Order"))
 
     class Meta:
         verbose_name = _("Job")
         verbose_name_plural = _("Jobs")
+        ordering = ("title",)
 
     def __str__(self):
         return self.title
