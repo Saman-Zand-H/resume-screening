048c55e24da2e975de23b67904fb138ffa2f3268
Saman Zand Haghighi <samanzandh@gmail.com>
2024-06-05 21:08:41 +0330

    fix: implement warning filter to suppress specific warning messages

--- django/apps/account/__init__.py
+++ django/apps/account/__init__.py
@@ -1,5 +1,7 @@
 import copy
 import functools
+import re
+import warnings
 
 from graphql_jwt import decorators
 
@@ -21,3 +23,32 @@ def __monkeypatch_graphql_jwt_datetime_func__():
 
 
 __monkeypatch_graphql_jwt_datetime_func__()
+
+
+class WarningFilter:
+    def __init__(self):
+        self.patterns = [
+            r"builtin type SwigPyPacked has no __module__ attribute",
+            r"builtin type SwigPyObject has no __module__ attribute",
+            r"builtin type swigvarlink has no __module__ attribute",
+        ]
+
+    def filter(self, message):
+        for pattern in self.patterns:
+            if re.search(pattern, message):
+                return True
+        return False
+
+
+class IgnoreSpecificWarnings:
+    def __init__(self):
+        self.warning_filter = WarningFilter()
+
+    def __call__(self, message, category, filename, lineno, file=None, line=None):
+        if self.warning_filter.filter(str(message)):
+            return
+
+        warnings.showwarning(message, category, filename, lineno, file, line)
+
+
+warnings.showwarning = IgnoreSpecificWarnings()
