cc03f57b2e14b055dce05e796c9baea79afb7a5b
Ali Avani <ali.avani@outlook.com>
2024-06-05 21:16:40 +0330

    feature: add middleware to disable introspection in production and update GraphQL view settings SCRUM-967

--- django/config/urls.py
+++ django/config/urls.py
@@ -1,16 +1,15 @@
-from account.views import TestView
 from apps.api.schema import schema
 from graphene_file_upload.django import FileUploadGraphQLView as GraphQLView
 
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
+    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=settings.DEBUG, schema=schema))),
     path("criteria/", include("criteria.urls"), name="criteria"),
     path("academy/", include("academy.urls"), name="academy"),
     path("media/", include("flex_blob.urls"), name="blob"),
-    path("test/", TestView.as_view()),
 ]
