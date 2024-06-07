e9d1e3bd4b980ae23559193e8598d89631980ba3
Ali Avani <ali.avani@outlook.com>
2024-06-07 11:52:16 +0330

    refactor: remove unused TestView and enhance email verification link styling

--- django/apps/account/views.py
+++ django/apps/account/views.py
@@ -1,9 +1,6 @@
 from dj_rest_auth.registration.views import SocialLoginView as BaseSocialLoginView
 
 from .adapters import GoogleOAuth2Adapter, LinkedInOAuth2Adapter
-from django.views import View
-from django.http import HttpResponse
-from cv.models import CVTemplate, GeneratedCV
 from .clients import OAuth2Client
 from .serializers import SocialLoginSerializer
 
@@ -22,8 +19,3 @@ class LinkedInOAuth2View(SocialLoginView):
     adapter_class = LinkedInOAuth2Adapter
     client_class = OAuth2Client
     callback_url = None
-
-
-class TestView(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse(CVTemplate.objects.first().render(GeneratedCV.get_user_context(self.request.user)))
