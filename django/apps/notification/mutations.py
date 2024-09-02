0856484c2ea71b428c0f442f1bddb1913014f1bc
Amin Amerian <aam.amin.am@gmail.com>
2024-09-02 13:40:15 +0330

    feature: don't update read_at for records which already set in InAppNotificationReadAtUpdateMutation SCRUM-1222

--- django/apps/notification/mutations.py
+++ django/apps/notification/mutations.py
@@ -14,7 +14,10 @@ class InAppNotificationReadAtUpdateMutation(DjangoBatchPatchMutation):
 
     @classmethod
     def before_mutate(cls, root, info, input):
+        ids = [item["id"] for item in input]
+        not_set_ids = InAppNotification.objects.filter(id__in=ids, read_at__isnull=True).values_list("id", flat=True)
         for item in input:
+            if int(item["id"]) in not_set_ids:
             item["read_at"] = timezone.now()
         return super().before_mutate(root, info, input)
 
