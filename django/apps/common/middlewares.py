b47826d13928f09e69ea79aabaaed94143cdf451
Ali Avani <ali.avani@outlook.com>
2024-11-30 11:55:52 +0330

    fix: streamline Sentry error attachment handling in middleware

--- django/apps/common/middlewares.py
+++ django/apps/common/middlewares.py
@@ -15,26 +15,30 @@ from .utils import map_exception_to_error
 graphql_logger = get_logger("graphql.error")
 
 
+def append_sentry_attachment(request, exception):
+    scope = get_current_scope()
+    scope.add_attachment(
+        bytes=ExceptionReporter(request, type(exception), exception, exception.__traceback__, is_email=False)
+        .get_traceback_html()
+        .encode("utf-8"),
+        filename=f"{type(exception).__name__}_{timezone.now().strftime('%Y-%m-%d-%H-%M-%S-%f')}.html",
+        content_type="text/html",
+    )
+
+
 class GrapheneErrorHandlingMiddleware:
-    def on_error(self, request, exc_type, exc_value, tb, error_kwargs):
+    def on_error(self, request, exception, error_kwargs):
         if settings.DEBUG:
             graphql_logger.error(
-                "".join(traceback.TracebackException.from_exception(exc_value).format()),
+                "".join(traceback.TracebackException.from_exception(exception).format()),
                 extra={"sentry_ignore": True},
             )
 
         if error_kwargs.get("error") is not Errors.INTERNAL_SERVER_ERROR:
             return
 
-        scope = get_current_scope()
-        scope.add_attachment(
-            bytes=ExceptionReporter(request, exc_type, exc_value, tb, is_email=False)
-            .get_traceback_html()
-            .encode("utf-8"),
-            filename=f"{exc_type.__name__}_{timezone.now().strftime('%Y-%m-%d-%H-%M-%S-%f')}",
-            content_type="text/html",
-        )
-        capture_exception(exc_value)
+        append_sentry_attachment(request, exception)
+        capture_exception(exception)
 
     def resolve(self, next_resolver, root, info, **args):
         try:
@@ -50,7 +54,7 @@ class GrapheneErrorHandlingMiddleware:
                 kwargs.update({"error": map_exception_to_error(e.__class__, str(e))})
             kwargs.update({"exception": e})
 
-            self.on_error(info.context, type(e), e, e.__traceback__, kwargs)
+            self.on_error(info.context, e, kwargs)
 
             raise base_exception(**kwargs)
 
@@ -64,11 +68,4 @@ class GrapheneDisableIntrospectionMiddleware:
 
 class DjangoErrorHandlingMiddleware(MiddlewareMixin):
     def process_exception(self, request, exception):
-        scope = get_current_scope()
-        scope.add_attachment(
-            bytes=ExceptionReporter(request, type(exception), exception, exception.__traceback__, is_email=False)
-            .get_traceback_html()
-            .encode("utf-8"),
-            filename=f"{type(exception).__name__}_{timezone.now().strftime('%Y-%m-%d-%H-%M-%S-%f')}",
-            content_type="text/html",
-        )
+        append_sentry_attachment(request, exception)
