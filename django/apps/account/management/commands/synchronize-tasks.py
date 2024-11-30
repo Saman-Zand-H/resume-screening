2817d57c4358f99d1cac92e3b496c09ff9e57db4
Saman Zand Haghighi <samanzandh@gmail.com>
2024-11-30 18:45:05 +0330

    refactor: update import for fields_join to fj for improved readability

--- django/apps/account/management/commands/synchronize-tasks.py
+++ django/apps/account/management/commands/synchronize-tasks.py
@@ -1,5 +1,5 @@
 from account.models import UserTask
-from common.utils import fields_join
+from common.utils import fj
 from flex_pubsub.tasks import task_registry
 
 from django.core.management import BaseCommand
@@ -9,7 +9,7 @@ from django.db.models.lookups import In
 class Command(BaseCommand):
     def handle(self, *args, **options):
         task_slugs = task_registry.get_all_tasks().keys()
-        matched_tasks = UserTask.objects.exclude(**{fields_join(UserTask.task_name, In.lookup_name): task_slugs})
+        matched_tasks = UserTask.objects.exclude(**{fj(UserTask.task_name, In.lookup_name): task_slugs})
         if not matched_tasks.exists():
             self.stdout.write("[!] No tasks found out of sync.", style_func=self.style.SUCCESS)
             return
