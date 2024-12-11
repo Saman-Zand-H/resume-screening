719dbf6b072eb6e04b6944d60d06bf7a42956a61
Saman Zand Haghighi <samanzandh@gmail.com>
2024-12-12 00:08:38 +0330

    fix: update user context key in JobAssessmentUserContextMixin for consistency

--- django/apps/criteria/mixins.py
+++ django/apps/criteria/mixins.py
@@ -2,4 +2,4 @@ from common.mixins import UserContextMixin
 
 
 class JobAssessmentUserContextMixin(UserContextMixin):
-    user_context_key = "job_assessment_user"
+    obj_context_key = "job_assessment_user"
